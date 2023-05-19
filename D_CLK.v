233531029f5ef63a7a4ce75dbdca2f7c7be6c541
AndyXZP <113650347+andy0119@users.noreply.github.com>
2023-05-19 15:53:55 +0800

    Add files via upload

---
+++ D_CLK.v
@@ -0,0 +1,12 @@
+module D_CLK(D,CLK,presetN,clearN,Q);
+input D,CLK,presetN,clearN;
+output reg Q; 
+
+always @(posedge CLK or negedge presetN or negedge clearN)
+	begin
+		if(~presetN) Q <= 1'b1;
+		else if (~clearN) Q <= 1'b0;
+		else Q <= D;
+	end
+endmodule
+		
